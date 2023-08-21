5a8da273c6222894ecd4b87ee090cd27830de43d
Nii Addo eagleaddo@yahoo.co.uk <eagleaddo@yahoo.co.uk>
2023-08-21 14:40:49 +0100

    watch list challenge part completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/lists', to: 'lists#index'
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
 end
